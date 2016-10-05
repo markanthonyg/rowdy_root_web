cbce8c1db730cbe29b09bea716231548671e7862
Jacob <jacobdaily737@gmail.com>
2016-10-05 11:29:18 -0500

    Add patients table back

--- resources/views/dashboard/patients.blade.php
+++ resources/views/dashboard/patients.blade.php
@@ -27,6 +27,7 @@
   <li class="crumb-link">
     <a href="/patients">Patients</a>
   </li>
+
 @endsection
 
 @section('content')
@@ -36,13 +37,13 @@
 
     {{-- ADD BUTTONS --}}
     <div class="col-md-4 hidden">
-      <a href="#" class="btn btn-lg btn-success"><span class="glyphicon glyphicon-plus-sign"></span> Add Patient</a>
+      <a href="/new_patient" class="btn btn-lg btn-success"><span class="glyphicon glyphicon-plus-sign"></span> Add Patient</a>
     </div>
 
     {{-- <br /><br /><br /> --}}
 
     {{-- SAMPLE TABLE --}}
-    <div class="col-md-12 hidden">
+    <div class="col-md-12">
       <div class="panel panel-visible" id="spy2">
         <div class="panel-heading">
           <div class="panel-title hidden-xs">
