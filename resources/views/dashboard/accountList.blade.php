614dacef4b454e180a87d8a9515a77ea27936ab5
jmarth <marth_joseph@yahoo.com>
2016-11-08 22:10:30 -0600

    small code clean up

--- resources/views/dashboard/accountList.blade.php
+++ resources/views/dashboard/accountList.blade.php
@@ -78,9 +78,6 @@
                     <td>{{$account->dob}}</td>
                     <td>{{$account->role}}</td>
                     <td>{{$account->email}}</td>
-
-                    <!--<td><a href="{{action('Dashboard\AccountController@deleteAccount')}}" class="delete_account btn btn-danger" data-id="{{ $account->id }}" name ="delete">
-                      <i class="fa fa-trash-o fa-lg"></i>  Delete</a></td>-->
                 </tr>
             @endforeach
             </tbody>
