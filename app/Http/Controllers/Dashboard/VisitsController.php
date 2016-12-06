bdf6966ad51a023b8b8809e325c06235df1fae25
Jacob Daily <Jacob Daily>
2016-12-05 22:13:16 -0600

    Began to write new visit backend

--- app/Http/Controllers/Dashboard/VisitsController.php
+++ app/Http/Controllers/Dashboard/VisitsController.php
@@ -29,6 +29,44 @@ class VisitsController extends Controller
 
     // Insert visit into DB
     public function insertVisit() {
+      // Logic will need to be added to pass $data and authenticate user
+
+      // Check if user is logged in, if not send back to home
+      if(!Auth::check()){
+        return Redirect::to('/');
+      }
+
+      /*
+        Collect variables from form
+      */
+      // Patient Demographics
+      $patient_id=0;
+      // Check if patient wants to remain anon
+      if(/* anon box is checked */){
+        /* patient will not have first name and last name */
+      } else {
+        $firstname = "";
+        $lastname = "";
+        // Other patient demo info
+      }
+      $chief_complaint = "";
+
+      // Examination
+      $vision_glasses_right = "";
+      $vision_glasses_left = "";
+      $vision_no_glasses_right = "";
+      $vision_no_glasses_left = "";
+
+      $glasses_rx_od_sphere = "";
+      $glasses_rx_od_cylinder = "";
+      $glasses_rx_od_axis = "";
+      $glasses_rx_od_add = "";
+      $glasses_rx_os_sphere = "";
+      $glasses_rx_os_cylinder = "";
+      $glasses_rx_os_axis = "";
+      $glasses_rx_os_add = "";
+      $glasses_rx_notes = "";
+
 
     }
 }
